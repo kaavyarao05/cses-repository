5d7084338ba969423a1f57a1da78d0031ebf5310
kaavyarao05 <kaavyarao05@gmail.com>
2025-02-26 00:26:05 +0530

    weird algo

--- weird_algo.cpp
+++ weird_algo.cpp
@@ -1,1 +1,28 @@
 // Make a file in your preferred language and start solving!
+
+#include<iostream>
+
+/*
+int weirdAlgo(int n){
+    std::cout<<n<<" ";
+    if(n==1) {return n;}
+    if(n&1) {weirdAlgo(n*3+1);}
+    else {weirdAlgo(n/2);}
+}
+*/
+
+void weirdAlgoLoop(unsigned long long int n){
+    while (n!=1){
+        std::cout<<n<<" ";
+        if(n&1)n=n*3+1;
+        else n/=2;
+    }
+    std::cout<<n<<" ";
+}
+
+int main(){
+    unsigned long long int n;
+    std::cin>>n;
+    weirdAlgoLoop(n);
+    return 0;
+}
